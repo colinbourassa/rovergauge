08d6b2976216db728c6266cdf7b9e521ab37d8cd
Colin Bourassa <colin.bourassa@gmail.com>
2013-11-10 18:26:33 -0500

    Horizontal stretch for fuel map columns

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -224,6 +224,7 @@ void MainWindow::setupWidgets()
     m_ui->m_fuelMapDisplay->resizeColumnsToContents();
     m_ui->m_fuelMapDisplay->resizeRowsToContents();
     m_ui->m_fuelMapDisplay->horizontalHeader()->setStyleSheet("QHeaderView { font-size: 9pt; }");
+    m_ui->m_fuelMapDisplay->horizontalHeader()->setResizeMode(QHeaderView::Stretch);
     unsigned int rowCount = m_ui->m_fuelMapDisplay->rowCount();
     unsigned int colCount = m_ui->m_fuelMapDisplay->columnCount();
     QTableWidgetItem *item = 0;
