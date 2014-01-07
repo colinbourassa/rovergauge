00713b969f96f65e2b6d503470206d71eed1b590
Colin Bourassa <colin.bourassa@gmail.com>
2014-01-06 21:17:31 -0500

    Win7/Qt5 build pain

--- make-mingw-debug.bat
+++ make-mingw-debug.bat
@@ -1,5 +1,11 @@
 @echo off
-cmake . -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug .
+call cmake . -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug
+if errorlevel 1 goto handleerror
 mingw32-make install
+if errorlevel 1 goto handleerror
 mingw32-make package
+goto done
+:handleerror
+echo Error, aborting build.
+:done
 pause
--- make-mingw-release.bat
+++ make-mingw-release.bat
@@ -1,5 +1,11 @@
 @echo off
-cmake . -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release .
+call cmake . -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release
+if errorlevel 1 goto handleerror
 mingw32-make install
+if errorlevel 1 goto handleerror
 mingw32-make package
+goto done
+:handleerror
+echo Error, aborting build.
+:done
 pause
